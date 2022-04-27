5216d1fa4a26c0a76370cc8c6ae0d99d5ef4ad71
develompent.j@outlook.com <development.j@outlook.com>
2022-04-27 20:11:01 -0300

    Project created

---
+++ src/main/java/com/jailtondomingos/cursospringSTS/CursospringstsApplication.java
@@ -0,0 +1,13 @@
+package com.jailtondomingos.cursospringSTS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursospringstsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursospringstsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jailtondomingos/cursospringSTS/CursospringstsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jailtondomingos.cursospringSTS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursospringstsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
