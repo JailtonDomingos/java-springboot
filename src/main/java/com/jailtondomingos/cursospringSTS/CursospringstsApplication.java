0d25930bef12e505385a7ca9a8243341a442fa39
develompent.j@outlook.com <development.j@outlook.com>
2022-04-28 15:46:07 -0300

    Database instance initialization created

--- src/main/java/com/jailtondomingos/cursospringSTS/CursospringstsApplication.java
+++ src/main/java/com/jailtondomingos/cursospringSTS/CursospringstsApplication.java
@@ -1,13 +1,33 @@
 package com.jailtondomingos.cursospringSTS;
 
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursospringstsApplication {
+import com.jailtondomingos.cursospringSTS.domain.Categoria;
+import com.jailtondomingos.cursospringSTS.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursospringstsApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository catergoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringstsApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		catergoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+		
 	}
 
 }
